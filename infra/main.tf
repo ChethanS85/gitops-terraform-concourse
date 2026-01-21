7e3fddba2e121ab5777c55da900b95279a639586
Chethan S <chethanshekarappa30@gmail.com>
2026-01-21 05:02:39 +0000

    Add infra folder with terraform code

---
+++ infra/main.tf
@@ -0,0 +1,15 @@
+terraform {
+    required_provider{
+        local={
+            source="hashicorp/local"
+        }
+    }
+}
+
+provider "local"{
+}
+
+resource "local_file" "example"{
+    filename="${path.module}/hello.txt"
+    content="Hello from Gitops"
+}
